147e9bf1fefd7fbfdc4900d291fc42e8ce83d198
Dean Sallinen <deza604@gmail.com>
2021-06-30 14:40:39 -0700

    Update App.svelte
    css

--- src/App.svelte
+++ src/App.svelte
@@ -13,23 +13,23 @@
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    display: flex;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  h1 {
+    font-size: 5vw;
+    font-weight: bold;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
