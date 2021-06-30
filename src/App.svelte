24de52d508769a5d44d4be819ffa259cc7e07fce
Dean Sallinen <deza604@gmail.com>
2021-06-30 14:32:32 -0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   		let maxlen = 8
       		let min = Math.pow(16,Math.min(len,maxlen)-1) 
       		let max = Math.pow(16,Math.min(len,maxlen)) - 1
-      		let n = Math.floor( Math.random() * (max-min+1) ) + min,
+      		let n = Math.floor( Math.random() * (max-min+1) ) + min
       		let r = n.toString(16);
   		while ( r.length < len ) {
      			r = r + randHex( len - maxlen );
