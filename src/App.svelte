4bdfe1ab8077bff3fbea32850efc820ef2accf47
Dean Sal <deza604@gmail.com>
2021-07-06 16:50:49 -0700

    add contrasting text color

--- src/App.svelte
+++ src/App.svelte
@@ -4,17 +4,27 @@
 	import { fade } from "svelte/transition";
 	const genHex = () => `#${Math.floor(Math.random() * 256**3).toString(16).padStart(6, '0')}`
 	let hex;
-	onMount(() => {
+	let textColor;
+	function getContrastYIQ(hexcolor){
+		hexcolor = hexcolor.replace("#", "");
+		var r = parseInt(hexcolor.substr(0,2),16);
+		var g = parseInt(hexcolor.substr(2,2),16);
+		var b = parseInt(hexcolor.substr(4,2),16);
+		var yiq = ((r*299)+(g*587)+(b*114))/1000;
+		return (yiq >= 128) ? '#333' : '#eee';
+	}
+onMount(() => {
     		confetti.create(document.getElementById("canvas"), {
       			resize: true,
       			useWorker: true
     		})({ particleCount: 200, spread: 200, origin: { y: 0.45 } });
     		hex = genHex();
+		textColor = getContrastYIQ(hex);
   	});
 </script>
 
 {#if hex}
-<main style="background:{hex}">
+<main style="background:{hex}; color:{textColor}">
     <h1 transition:fade={{ duration: 200 }}>{hex}</h1>
 </main>
   {/if}
@@ -40,3 +50,4 @@
     }
   }
 </style>
+
